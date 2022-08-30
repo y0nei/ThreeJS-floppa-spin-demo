d7545ff0a2372a12fc8c58d1f84ae13680155fff
y0nei <xyz.im@proton.me>
2022-08-31 00:06:21 +0200

    Three.js: Add support for multiple models

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
 import {GLTFLoader} from "./js/GLTFLoader.js";
 
+var lightColor = 0xffffff;
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-    100,
-    window.innerWidth/window.innerHeight,
-    0.1,
-    1000
-);
-var renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+var camera = new THREE.PerspectiveCamera(100, window.innerWidth/window.innerHeight, 0.1, 1000);
+var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setViewport(0, 200, window.innerWidth, window.innerHeight); // Center everything
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setViewport(0, 200, window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var obj;
-var loader = new GLTFLoader();
-loader.load('./models/floppa.gltf', function(gltf){
-    obj = gltf.scene;
-    gltf.scene.traverse( function( node ) {
-        if ( node.isMesh ) { node.castShadow = true; }
+function loadModel(url) {
+    return new Promise(resolve => {
+      new GLTFLoader().load(url, resolve);
     });
-    scene.add(gltf.scene);
+}
+
+let floppaModel, radioModel;
+let m1 = loadModel('models/floppa.gltf').then(result => {  floppaModel = result.scene.children[0]; });
+let m2 = loadModel('models/radio.gltf').then(result => {  radioModel = result.scene.children[0]; });
+
+Promise.all([m1,m2]).then(() => {
+    // floppaModel.position.set(0,0.034,0);
+    floppaModel.position.set(0,0.025,0);
+    radioModel.position.set(1.4,-0.5,1.2);
+    radioModel.rotation.z = 0.3;
+
+    // Model shadows
+    floppaModel.castShadow = true;
+    radioModel.traverse(function(node){
+        if (node.isMesh){ node.castShadow = true; }
+    })
+    
+    scene.add(floppaModel);
+    scene.add(radioModel);
+    
+    renderer.render(scene,camera);
 });
 
-var spotLight = new THREE.SpotLight(0xffffff);
-spotLight.position.set( 0, 10, 5 );
+var spotLight = new THREE.SpotLight(lightColor);
+spotLight.position.set(0, 10, 5);
 spotLight.angle = 0.23;
 spotLight.shadowMapVisible = true;
 spotLight.castShadow = true;
-spotLight.penumbra = 0.3;
-spotLight.intensity = 1.5;
+spotLight.penumbra = 0.5;
+spotLight.intensity = 2;
 scene.add(spotLight);
 
-var light = new THREE.AmbientLight(0xffffff, 1);
+var light = new THREE.AmbientLight(lightColor, 1);
 scene.add(light);
 
-const planeGeometry = new THREE.PlaneGeometry( 10, 15 );
-const planeMaterial = new THREE.MeshStandardMaterial( { color: 0x242424, side: THREE.DoubleSide } )
-const plane = new THREE.Mesh( planeGeometry, planeMaterial );
+const planeGeometry = new THREE.PlaneGeometry(10, 15);
+const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x242424, side: THREE.DoubleSide })
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.receiveShadow = true;
-plane.rotation.x = 1.6;
-plane.position.y = -0.3;
+plane.rotation.x = Math.PI / 2;
+plane.position.y = -0.5;
 plane.position.z = 0;
-scene.add( plane );
+scene.add(plane);
 
 camera.position.set(0,2,5);
 function animate() {
     requestAnimationFrame(animate);
-    obj.rotation.y += 0.01;
+    floppaModel.rotation.y += 0.01;
     renderer.render(scene,camera);
 }
 animate();
