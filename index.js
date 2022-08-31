90a50a53b7cb864cbda287405f800aab2a84fb37
y0nei <xyz.im@proton.me>
2022-08-31 12:39:26 +0200

    Three.js: add click event and raycaster to radio

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 import {GLTFLoader} from "./js/GLTFLoader.js";
 
 var lightColor = 0xffffff;
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(100, window.innerWidth/window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+var renderer = new THREE.WebGLRenderer({ 
+    antialias: true, 
+    alpha: true 
+});
+
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setViewport(0, 200, window.innerWidth, window.innerHeight); // Center everything
+// renderer.setViewport(0, 200, window.innerWidth, window.innerHeight); // Center everything
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
+// Multiple model loading
 function loadModel(url) {
     return new Promise(resolve => {
-      new GLTFLoader().load(url, resolve);
+        new GLTFLoader().load(url, resolve);
     });
 }
 
@@ -25,19 +32,36 @@ Promise.all([m1,m2]).then(() => {
     floppaModel.position.set(0,0.025,0);
     radioModel.position.set(1.4,-0.5,1.2);
     radioModel.rotation.z = 0.3;
-
-    // Model shadows
+    
     floppaModel.castShadow = true;
     radioModel.traverse(function(node){
-        if (node.isMesh){ node.castShadow = true; }
+        if (node.isMesh){ 
+            node.castShadow = true; 
+        }
     })
     
     scene.add(floppaModel);
     scene.add(radioModel);
-    
-    renderer.render(scene,camera);
+    renderer.render(scene,camera);    
 });
 
+// Mouse click event -> raycaster 
+renderer.domElement.addEventListener('click', onClick, false);
+function onClick( event ) {
+    event.preventDefault();
+    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    
+    raycaster.setFromCamera( mouse, camera );
+    var intersects = raycaster.intersectObject( radioModel );
+    
+    var clicked = false;
+    if ( intersects.length > 0 ) {
+        alert("radioClick")
+    }
+}
+
+// Spotlight
 var spotLight = new THREE.SpotLight(lightColor);
 spotLight.position.set(0, 10, 5);
 spotLight.angle = 0.23;
@@ -47,9 +71,11 @@ spotLight.penumbra = 0.5;
 spotLight.intensity = 2;
 scene.add(spotLight);
 
+// Ambient Light
 var light = new THREE.AmbientLight(lightColor, 1);
 scene.add(light);
 
+// Plane
 const planeGeometry = new THREE.PlaneGeometry(10, 15);
 const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x242424, side: THREE.DoubleSide })
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
